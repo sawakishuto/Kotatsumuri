ce6673a130d7d525d41a6bcc63ca4306f970d240
punksguitar <hanahanaogutaro@gmail.com>
2024-12-20 23:50:31 +0900

    モデルの作成、マイグレーション、コントローラー作成

---
+++ app/models/users_plant.rb
@@ -0,0 +1,2 @@
+class UsersPlant < ApplicationRecord
+end
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,4 +5,4 @@
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
   :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
-]
+] 
