c40e9094f82d32ba6e02496dfcba85825f99a9fd
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-27 18:04:00 +0900

    fix: さらに修正

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,5 @@
 class Plant < ApplicationRecord
   # ネストされた属性を受け入れる
-  accepts_nested_attributes_for :growth_condition, :care_schedule, :propagation_methods, :steps
+  accepts_nested_attributes_for :growth_condition, :care_schedule, :propagation_methods
+
 end
