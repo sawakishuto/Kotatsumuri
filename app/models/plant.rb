a70458991d9c772a76fe8e35a695a92a559e5fab
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-27 18:00:32 +0900

    fix: Userクラスを修正

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,20 +1,4 @@
 class Plant < ApplicationRecord
   # ネストされた属性を受け入れる
   accepts_nested_attributes_for :growth_condition, :care_schedule, :propagation_methods, :steps
-
-
-  # 手順
-  class Step < ApplicationRecord
-    belongs_to :propagation_method
-  end
-
-  # 施肥期間
-  class FertilizingPeriod < CarePeriod
-    attribute :frequency, :string
-  end
-
-  # 剪定期間
-  class PruningPeriod < CarePeriod
-    attribute :description, :string
-  end
 end
