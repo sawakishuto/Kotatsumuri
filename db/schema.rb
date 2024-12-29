538b42e90f3a07ae45ad8528b2489699b3e6d635
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-29 14:52:03 +0900

    fix: 整合性を合わせるために修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_27_091040) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_29_142024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_27_091040) do
     t.string "line_user_id"
     t.string "name"
     t.string "profile_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users_plants", force: :cascade do |t|
+    t.string "uid"
+    t.string "plantid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
