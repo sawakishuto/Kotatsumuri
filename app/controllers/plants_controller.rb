bd7ee0983cf4f79a49e4433d94c8fb8c1c3d0518
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-29 16:37:12 +0900

    feat: 基本情報を取得し表示できるようにした

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,19 @@
 class PlantsController < ApplicationController
+
+  def index
+    query = params[:query]
+    @plants = Plant.where("name LIKE ?", "%#{query}%").first
+    @care_schedules = CareSchedule.where("plant_id = ?", @plants.id).first
+    @care_periods = CarePeriod.where("care_schedule_id = ?", @care_schedules.id)
+    @growth_conditions = GrowthCondition.where("plant_id = ?", @plants.id)
+    render json: {
+      plant: {
+        name: @plants.name,
+        description: @plants.description,
+        care_schedules: @care_schedules,
+        care_periods: @care_periods,
+        growth_conditions: @growth_conditions
+  }
+    }
+  end
 end
