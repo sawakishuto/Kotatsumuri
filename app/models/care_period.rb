80d4652b9f9441a88fd9ceb1e9f961d423b46f3e
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-11 17:38:17 +0900

    fix: 関連性修正

--- app/models/care_period.rb
+++ app/models/care_period.rb
@@ -1,7 +1,7 @@
   # ケア期間
   class CarePeriod < ApplicationRecord
-    belongs_to :care_schedule
-    validates :period_type, inclusion: { in: %w[blooming planting repotting fertilizing pruning] }
+    belongs_to :plant
+    validates :period_type, inclusion: { in: %w[blooming_period planting_period repotting_period fertilizing_period pruning_period] }
 
     validates :start_date, :end_date, presence: true
   end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,6 @@
 class Plant < ApplicationRecord
     # 他のアソシエーション
     has_one :growth_condition
-    has_one :care_schedule
+    has_many :care_periods
     has_many :propagation_methods
 end
