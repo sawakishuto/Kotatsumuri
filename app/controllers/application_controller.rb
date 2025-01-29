82c98b3b76bb5291f64ebffe015694d355758bd6
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-29 22:26:00 +0900

    feat: 認証処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_firebase_user
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_firebase_user
+    header = request.headers["Authorization"]
+    return unauthorized unless header.present?
+
+    token = header.split(" ").last
+    begin
+      peyload = FirebaseIdToken::Signature.verify(token)
+
+      firebase_uid = payload['sub']
+
+      @current_user = User.find_or_create_by!(firebase_uid: firebase_uid) do |u|
+        u.email = user_email
+        # 他にも必要な情報があればここでセット
+      end
+    rescue => e
+      # 検証エラー時は 401 を返す
+      Rails.logger.warn "Firebase auth error: #{e.message}"
+      return unauthorized
+    end
+  end
+  
+  def unauthorized
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
+
 end
