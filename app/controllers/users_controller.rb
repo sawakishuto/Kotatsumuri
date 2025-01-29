f6e85a2a6f715fa5a3eed086048a5ef4bbe3f2e3
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-29 22:27:06 +0900

    usercontrollerを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
-  
+  def update
+    user = User.find(@current_user.id)
+    user.update_attribute(
+      name: params[:name],
+    )
+    render json: user
+  end
 end
