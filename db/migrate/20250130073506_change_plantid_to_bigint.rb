f29e267bbdde37b5aa34a4d58c56de193ea8718d
hiromuota166 <otahiromu166@gmail.com>
2025-01-30 17:02:24 +0900

    メモ

--- db/migrate/20250130073506_change_plantid_to_bigint.rb
+++ db/migrate/20250130073506_change_plantid_to_bigint.rb
@@ -1,4 +1,5 @@
 class ChangePlantidToBigint < ActiveRecord::Migration[7.2]
+  # PostgreSQLでは、型の互換性がない場合、手動で USING を指定する必要がある
   def up
     change_column :users_plants, :plantid, 'bigint USING plantid::bigint'
   end
