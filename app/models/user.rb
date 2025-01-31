f67d0d3dd41babd2df48c17d9a0c68be70054f81
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-31 16:29:37 +0900

    fix: バリデーションや設定を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[line] # この1行を追加
+  :recoverable, :rememberable,:omniauthable, :validatable
+
+  validates :email, presence: false, allow_nil: true
+  validates :encrypted_password, presence: false, allow_nil: true
+
+  # Firebase UIDを必須にする
+  validates :firebase_uid, presence: true, uniqueness: true
 end
