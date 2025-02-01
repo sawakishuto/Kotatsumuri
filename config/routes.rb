7903f107529cd1046f4a891be14e82b5543a0ce0
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-02 00:43:48 +0900

    feat: エンドポイントを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users_plants
-  resources :users
+  resources :users, only: [ :update, :show ] do
+    collection do
+      patch "me", to: "users#update_me"
+    end
+  end
   resources :plants do
-    resources :search, only: [:show]
+    resources :search, only: [ :show ]
   end
   # Defines the root path route ("/")
   # root "posts#index"
