5ec1b7407383fb61ec362e8e704ecb41a029822e
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-06 04:11:36 +0900

    fix: プロパティをしゅうせう

--- app/controllers/users/plants_controller.rb
+++ app/controllers/users/plants_controller.rb
@@ -3,12 +3,11 @@ class PlantsController < ApplicationController
   def create
     begin
       UsersPlant.create!(
-        plant_id=params[:plant_id],
-        users_id=@current_user.firebase_uid
+        uid: @current_user.firebase_uid,
+        plantid: params[:plant_id]
       )
-
     rescue
-        render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
+        render json: { error: "保存に失敗しました"  }, status: :unprocessable_entity
     end
   end
 
@@ -18,11 +17,11 @@ class PlantsController < ApplicationController
   def  index
     begin
      users_id=@current_user.firebase_uid
-     plants_ids=UsersPlant.where(uid: users_id).pluck(:pid)
-     plants=Plant.where(pid: plants_ids)
-     render json: {plants: plants}
+     plants_ids=UsersPlant.where(uid: users_id).pluck(:id)
+     plants=Plant.where(id: plants_ids)
+     render json: { plants: plants }
     rescue
-      render json: {error: "見つかりませんでした"}
+      render json: { error: "見つかりませんでした" }
     end
   end
 
