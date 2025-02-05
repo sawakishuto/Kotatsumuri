39e5d86a6e5cca069790a3ff9394a497ff0dc248
punksguitar <hanahanaogutaro@gmail.com>
2025-02-06 03:11:54 +0900

    fix 関連定義の修正

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,6 @@ class Plant < ApplicationRecord
   has_one :growth_condition
   has_many :care_periods
   has_many :propagation_methods
+  has_many :users, through: :users_plants
+  has_many :users_plants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
+has_many :plants, through: :users_plants
+has_many :users_plants
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable,:omniauthable, :validatable
 
