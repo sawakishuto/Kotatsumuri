514aabffbfbb88eb53aed62eb215f450dd8d10f6
hiromuota166 <otahiromu166@gmail.com>
2025-02-09 22:47:22 +0900

    add: plantsモデルにwikiAPIから画像を取得する関数作成

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,8 +1,42 @@
 class Plant < ApplicationRecord
-  
+
   has_one :growth_condition
   has_many :care_periods
   has_many :propagation_methods
   has_many :users, through: :users_plants
   has_many :users_plants
+
+  def self.search_wikipedia_image_url(search_key)
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    base_url = 'https://ja.wikipedia.org/w/api.php'
+
+    query_params = {
+      action: 'query',
+      title: search_key,
+      prop: 'pageimages',
+      pithumbsize: 300,
+      format: 'json'
+    }
+
+    uri = URI(base_url)
+    uri.query = URI.encode_www_form(query_params)
+
+    response = Net::HTTP.get(uri)
+
+    parsed = JSON.parse(response)
+
+    pages_data = parsed.dig('query', 'pages')
+    return nil if pages_data.nil?
+
+    page = pages_data.values.first
+    return nil if page.nil?
+
+    thumbnail_info = page['thumbnail']
+    return nil if thumbnail_info.nil?
+
+    thumbnail_info['source']
+  end
 end
