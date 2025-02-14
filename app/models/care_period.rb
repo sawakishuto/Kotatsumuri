212dcfd03d66d6826184b7292cc5e5d80ca97b5b
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-15 02:13:06 +0900

    fix: フェーズ表示方法を修正

--- app/models/care_period.rb
+++ app/models/care_period.rb
@@ -1,12 +1,14 @@
 class CarePeriod < ApplicationRecord
-  belongs_to :plant
-  validates :period_type, inclusion: { in: %w[blooming_period planting_period repotting_period fertilizing_period pruning_period] }
-  validates :start_date, :end_date, presence: true
-
   # 検索日時が start_date < 今日 < end_date に該当するデータを取得
   def self.for_plant_ids_in_date_range(plant_ids, search_date)
-    puts("search_date: #{search_date}")
-    where(plant_id: plant_ids)
-      .where("start_date <= ? AND end_date >= ?", search_date, search_date)
+    month_day = search_date.strftime("%m-%d")
+    puts("search_date (month-day): #{month_day}")
+
+    where(plant_id: plant_ids).where(
+      "(to_char(start_date, 'MM-DD') <= ? AND to_char(end_date, 'MM-DD') >= ?) OR " \
+      "(to_char(start_date, 'MM-DD') > to_char(end_date, 'MM-DD') AND " \
+      "(to_char(start_date, 'MM-DD') <= ? OR to_char(end_date, 'MM-DD') >= ?))",
+      month_day, month_day, month_day, month_day
+    )
   end
 end
