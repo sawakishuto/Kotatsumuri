1a137ef6e2339e58e38e2ce0d5fde9dae775552a
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-15 02:13:40 +0900

    fix: 診断周りの修正

---
+++ app/models/diagnosis.rb
@@ -0,0 +1,4 @@
+class Diagnosis < ApplicationRecord
+  has_one_attached :image
+
+end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,5 @@
 class Plant < ApplicationRecord
-
+  has_one_attached :image
   has_one :growth_condition
   has_many :care_periods
   has_many :propagation_methods
