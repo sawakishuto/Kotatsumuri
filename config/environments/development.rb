5ee91b34689e68d533203986f87f8e26bf2f2abd
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-15 02:11:12 +0900

    fix: GCSの設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.active_storage.service = :google
 
   #ngrokの許可
   config.hosts.clear # 全てのホストを許可
@@ -32,7 +33,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
