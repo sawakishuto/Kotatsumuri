0da707d5a94ae35689cacaebdc2515e12a0aac74
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-22 14:57:04 +0900

    feat: VOの実装

---
+++ app/domain/value_object/propagation_methods.rb
@@ -0,0 +1,21 @@
+  module ValueObject
+    class ValueObject::PropagationMethods < ValueObject::Base
+      attr_reader :plant_id, :method, :description
+
+      validates :plant_id, presence: true
+      validates :method, presence: true
+      validates :description, presence: true
+
+      def initialize(plant_id, method, description)
+        @plant_id = plant_id
+        @method = method
+        @description = description
+        super()
+      end
+
+      private
+      def evaluate_attributes
+        %i[plant_id method description]
+      end
+    end
+  end
